d83fdf730add82e2e60279735ec198e282b003b7
Dominic Tarr <dominic.tarr@gmail.com>
2016-09-23 22:33:28 +1200

    better tests for abort

--- test/abort.js
+++ test/abort.js
@@ -1,7 +1,8 @@
 var pull    = require('pull-stream')
 var through = require('through')
 var toPull  = require('../')
-
+var Hang = require('pull-hang')
+var Cat = require('pull-cat')
 
 require('tape')('abort', function (t) {
 
@@ -32,3 +33,43 @@ require('tape')('abort', function (t) {
     }
   )
 })
+
+require('tape')('abort hang', function (t) {
+
+  var ts = through(), aborted = false, c = 0, _read, ended, closed
+  ts.on('close', function () {
+    closed = true
+  })
+  pull(
+    Cat([
+      pull.values([.1, .4, .6, 0.7, 0.3]),
+      Hang(function () {
+        aborted = true
+      })
+    ]),
+    toPull(ts),
+    function (read) {
+      _read = read
+      read(null, function next (end, data) {
+        if(end) {
+          ended = true
+        }
+        else read(null, next)
+      })
+    }
+  )
+
+  setTimeout(function () {
+    _read(true, function (end) {
+      t.ok(aborted, 'aborted')
+      t.ok(ended, 'ended')
+      t.ok(closed, 'closed')
+      t.ok(end, 'abort cb end')
+      t.end()
+    })
+  }, 10)
+})
+
+
+
+
